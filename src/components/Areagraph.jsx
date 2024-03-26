d8f0ad386590fa59795924846b06872702540d6c
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-27 01:42:06 +0530

    implemented area graph

--- src/components/Areagraph.jsx
+++ src/components/Areagraph.jsx
@@ -1,62 +1,50 @@
 /* eslint-disable react/prop-types */
 
+import Chart from "react-apexcharts";
 
-import Chart from 'react-apexcharts';
-
-const Areagraph = ({ data, legend = false ,text}) => {
-  
-
+const Areagraph = ({ data, legend = false, text }) => {
   const options = {
     legend: {
-        show: legend,
-      },
+      show: legend,
+    },
     chart: {
-      type: 'area', // Change chart type to area for Areagraph
+      type: "area",
       height: 250,
-      foreColor: '#333', // Text color
+      foreColor: "#333",
       toolbar: {
-        show: false // Hide the toolbar
-      }
+        show: false,
+      },
     },
     dataLabels: {
       enabled: false,
     },
     stroke: {
-      curve: 'smooth', // Use smooth curve for Areagraph
+      curve: "smooth",
     },
-    title: {
-      text: 'Student Marks Trend',
-      align: 'left',
-    },
+
     xaxis: {
-     
+      
       title: {
         text: text,
       },
     },
-    yaxis: {
-      title: {
-        text: 'Marks',
-      },
-    },
+
     grid: {
-      borderColor: '#042F2E', // Border color
-      strokeDashArray: 0, // Remove stroke dashes
-      position: 'back', // Move the grid to the back
+      borderColor: "#042F2E",
+      strokeDashArray: 0,
+      position: "back",
       xaxis: {
         lines: {
-          show: false // Hide x-axis lines
-        }
+          show: false,
+        },
       },
       yaxis: {
         lines: {
-          show: false // Hide y-axis lines
-        }
-      }
+          show: false,
+        },
+      },
     },
   };
-
- 
 
   return (
     <div className="size-40 border-4 border-purple-600 rounded-lg m-2 border-#042F2E">
@@ -66,3 +54,5 @@ const Areagraph = ({ data, legend = false ,text}) => {
 };
 
 export default Areagraph;
+
+
