ea95b0ad137a812f3c72586e922e5d209e2988d2
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-03-27 01:38:30 +0530

    formatted bargraph

--- src/components/Bargraph.jsx
+++ src/components/Bargraph.jsx
@@ -2,15 +2,34 @@
 
 import Chart from "react-apexcharts";
 
-const Bargraph = ({ data, legend = false, categories ,text}) => {
-
+const Bargraph = ({ data, legend, categories, text }) => {
   const options = {
     legend: {
-      show: legend,
+      show: legend ? legend : true,
+      fontSize: "10px",
+      fontWeight: "bold",
+
+      marginTop: "10px",
+      formatter: function (value) {
+        return value.split(" ")[0] + " d";
+      },
+      itemMargin: {
+        horizontal: 8,
+        vertical: 10
+    },
+    labels: {
+      colors: "white",
+      // useSeriesColors: false
+  },
     },
     chart: {
       type: "bar",
       height: 250,
+      width: 500,
+      toolbar: {
+        show: false,
+        autoSelected: "zoom",
+      },
     },
     plotOptions: {
       bar: {
@@ -20,23 +39,82 @@ const Bargraph = ({ data, legend = false, categories ,text}) => {
         },
       },
     },
+    axisBorder: {
+      show: true,
+      color: "#78909C",
+      height: 1,
+      width: "100%",
+      offsetX: 0,
+      offsetY: 0,
+    },
     dataLabels: {
-      enabled: true,
-      offsetY: -20,
-      style: {
-        fontSize: "12px",
-        colors: ["#333"],
-      },
+      enabled: false,
     },
     xaxis: {
       categories: categories || [],
       title: {
         text: text,
+        offsetY: 5,
+        offsetX: -12,
+        style: {
+          color: "white",
+          fontSize: "10px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
+          // 
+         
+        },
+      },
+      labels: {
+        show: true,
+        formatter: function (value) {
+          return value.split(" ")[0] + " d";
+        },
+        style: {
+          colors: "white",
+          fontSize: "8px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
+        },
+        offsetY: -5,
+      },
+      axisBorder: {
+        show: true,
+        color: "white",
+        height: 1,
+        width: "100%",
+        offsetX: 8,
+        offsetY: 0,
+      },
+      axisTicks: {
+        show: true,
+        borderType: "solid",
+        color: "#78909C",
+        height: 4,
+        offsetX: 8,
+        offsetY: 0,
       },
     },
     yaxis: {
       title: {
         text: "Reports",
+        offsetX: 3,
+        style: {
+          color: "white",
+          fontSize: "10px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
+        },
+      },
+      stepSize: 10,
+      labels: {
+        show: true,
+        style: {
+          colors: "white",
+          fontSize: "8px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: 600,
+        },
       },
     },
     grid: {
@@ -51,12 +129,24 @@ const Bargraph = ({ data, legend = false, categories ,text}) => {
           show: false,
         },
       },
+      padding: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
     },
   };
 
   return (
-    <div className="size-40 border-4 border-purple-600 rounded-lg m-2">
-      <Chart options={options} series={data} type="bar" height={180} />
+    <div className="size-52 ring-4 ring-purple-500 rounded-xl my-8">
+      <Chart
+        options={options}
+        series={data}
+        type="bar"
+        height={265}
+        width={200}
+      />
     </div>
   );
 };
