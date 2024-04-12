d6811aadad4b4d2d1286233c546bd1319dd64dac
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-12 16:21:11 +0530

    update: background color and text color when hover

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,13 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className=" shadow sticky z-50 top-0 ">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 flex justify-center">
-        {/* <div className="flex flex-wrap justify-between items-center mx-auto max-w-sm">
-          <Link to="/" className="flex items-center">
-            <img src="" alt="logo" className="mr-3 h-12" />
-          </Link>
-        </div> */}
-
+    <header className=" shadow sticky z-50 m-4 top-0 ">
+      <nav className="bg-black border-sky-700 px-4 lg:px-6 py-2.5 flex justify-center shadow-md">
         <div
           className=" hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
           id="mobile-menu-2"
         >
-          <ul className="flex gap-5">
+          <ul className="flex gap-5 ">
             <li>
               <NavLink
                 to="/intheft"
@@ -22,9 +16,9 @@ function Header() {
                   `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-orange-700"
-                                            : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            ? "text-blue-700"
+                                            : "text-white"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-purple-700 lg:p-0`
                 }
               >
                 inTheft
@@ -37,9 +31,9 @@ function Header() {
                   `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-orange-700"
-                                            : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            ? "text-purple-700"
+                                            : "text-white"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-purple-700 lg:p-0`
                 }
               >
                 devices
@@ -52,9 +46,9 @@ function Header() {
                   `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-orange-700"
-                                            : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            ? "text-purple-700"
+                                            : "text-white"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-purple-700 lg:p-0`
                 }
               >
                 Vehicles
@@ -67,9 +61,9 @@ function Header() {
                   `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-orange-700"
-                                            : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            ? "text-purple-700"
+                                            : "text-white"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-purple-700 lg:p-0`
                 }
               >
                 My Customers
@@ -82,9 +76,9 @@ function Header() {
                   `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-orange-700"
-                                            : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            ? "text-purple-700"
+                                            : "text-white"
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-purple-700 lg:p-0`
                 }
               >
                 Profile
